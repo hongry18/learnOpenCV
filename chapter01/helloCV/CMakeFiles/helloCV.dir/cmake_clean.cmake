8681e980ce61e1e8d881d75c6e3da782318d21a1
hongry <hongry@Macintosh.local>
2019-07-24 11:04:33 +0900

    edit gitignore

---
+++ chapter01/helloCV/CMakeFiles/helloCV.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloCV.dir/main.cpp.o"
+  "helloCV.pdb"
+  "helloCV"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloCV.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
